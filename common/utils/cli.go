0460dd575298446ca38a66f0597f4a5e63972447
wangyanyo <2378130965@qq.com>
2024-05-16 22:38:02 +0800

    add utils

---
+++ common/utils/cli.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/wangyanyo/21point/common/enum"
+)
+
+func Cle() {
+	cmd := &exec.Cmd{}
+	if enum.SYS_TYPE == "windows" {
+		cmd = exec.Command("cmd.exe", "/c", "cls")
+	}
+	if enum.SYS_TYPE == "linux" {
+		cmd = exec.Command("sh", "-c", "clear")
+		fmt.Println(cmd)
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
+
+func MD5(str string) string {
+	h := md5.New()
+	h.Write([]byte(str))
+	return hex.EncodeToString(h.Sum(nil))
+}
