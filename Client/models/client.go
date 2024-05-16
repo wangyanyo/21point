6d1198de3d6b49240be01b40e0e26cba16e816e3
wangyanyo <2378130965@qq.com>
2024-05-16 20:55:13 +0800

    debug

--- Client/models/client.go
+++ Client/models/client.go
@@ -1,1 +1,34 @@
 package models
+
+import (
+	"net"
+
+	"github.com/wangyanyo/21point/common/entity"
+)
+
+var Rconn = make(chan bool)
+
+type TcpClient struct {
+	Connection net.TCPConn
+	HawkServer net.TCPAddr
+	StopChan   chan struct{}
+	CmdChan    chan *entity.TransfeData
+	Token      string
+}
+
+func (c *TcpClient) Send(b []byte) (int, error) {
+	return c.Connection.Write(b)
+}
+
+func (c *TcpClient) Read(b []byte) (int, error) {
+	return c.Connection.Read(b)
+}
+
+func (c *TcpClient) Addr(b []byte) string {
+	return c.Connection.RemoteAddr().String()
+}
+
+func (c *TcpClient) Close() {
+	c.Connection.Close()
+	Rconn <- true
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module github.com/wangyano/21point
+module github.com/wangyanyo/21point
 
 go 1.22.1
-
-require github.com/wangyanyo/21point v0.0.0-20240516102646-bfc8b46579bb // indirect
