139808ad7e8c463966a9b1a6afcc9fb5274eab82
2378130965 <2378130965@MacBook-Pro-101.local>
2024-07-05 03:45:52 -0700

    更新需求

---
+++ Server/models/User.go
@@ -0,0 +1,19 @@
+package models
+
+import "time"
+
+type User struct {
+	Id         int       `grom:"primary_key;auto_increment" json:"id"`
+	UserName   string    `grom:"column:user_name;unique" json:"user_name"`
+	Password   string    `grom:"column:password" json:"password"`
+	CreateTime time.Time `gorm:"column:create_time;autoCreateTime" json:"create_time"` // 创建时间
+	UpdateTime time.Time `gorm:"column:update_time;autoUpdateTime" json:"update_time"` // 更新时间
+}
+
+func (user *User) TableName() string {
+	return "tbl_user"
+}
+
+func (user *User) InitTable() {
+
+}
