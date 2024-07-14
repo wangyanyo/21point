ebaafcaada4fbec93822df54bdccea70f4413c56
wangyanyo <2378130965@qq.com>
2024-07-14 14:00:25 +0800

    更新

--- Server/models/User.go
+++ Server/models/User.go
@@ -11,6 +11,7 @@ type User struct {
 	Id         int       `grom:"primary_key;auto_increment" json:"id"`
 	UserName   string    `grom:"column:user_name;unique" json:"user_name"`
 	Password   string    `grom:"column:password" json:"password"`
+	Score      string    `grom:"column:score" json:"score"`
 	CreateTime time.Time `gorm:"column:create_time;autoCreateTime" json:"create_time"` // 创建时间
 	UpdateTime time.Time `gorm:"column:update_time;autoUpdateTime" json:"update_time"` // 更新时间
 }
--- Server/tcpsrc/router.go
+++ Server/tcpsrc/router.go
@@ -1,7 +1,19 @@
 package tcpsrc
 
-import "github.com/wangyanyo/21point/common/entity"
+import (
+	"github.com/wangyanyo/21point/Sever/controller"
+	"github.com/wangyanyo/21point/common/entity"
+	"github.com/wangyanyo/21point/common/enum"
+)
 
 func Router(req *entity.TransfeData) {
+	switch req.Cmd {
+	case enum.HeartPacket:
+		controller.HeartHandle()
 
+	case enum.RegisterPacket:
+
+	case enum.LoginPacket:
+
+	}
 }
